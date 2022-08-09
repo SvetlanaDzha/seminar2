9704faad3a83cc65e416d73833b25c978a6088ae
Svetlana <svetlanka2503@mail.ru>
2022-08-09 19:56:38 +0300

    Task09

---
+++ Task09/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
+// 12 -> 2
+// 78 -> 8
+// 85 -> 8
+
+//Random rand = new Random();
+
+int number = new Random().Next(10, 100);
+Console.WriteLine(number);
+int digit2 = number % 10;
+int digit1 = number / 10;
+if (digit1 > digit2)
+{
+    Console.Write(digit1);
+}
+else
+{
+    Console.Write(digit2);
+}
+Console.WriteLine(" - наибольшая цифра");
